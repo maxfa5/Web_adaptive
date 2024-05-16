77f643768adcb7c06738375687e2f94b73aafbaa
Sandajer <maksfa04@mail.ru>
2024-05-16 18:48:20 +0500

    header+promo

---
+++ lab2/src/App.vue
@@ -0,0 +1,24 @@
+<script >
+import VHeader from '@/components/VHeader.vue';
+import VFooter from '@/components/VFooter.vue';
+import VService from '@/components/VService.vue';
+
+export default {
+ components: {
+  VHeader,
+  VService,
+  VFooter
+ }
+}
+</script>
+
+<template>
+  <VHeader></VHeader>
+  <VService></VService>
+  <VFooter></VFooter>
+</template>
+
+
+<style>
+
+</style>
---
+++ lab2/src/components/VService.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <section class="service">
+            <div class="row">
+                <div class="service__content">
+                    <div class="col-12 col-xl-5 service__text">
+                        <h2>Сервис для проведения онлайн-занятий</h2>
+
+                        <p>Проводите видеоуроки для всего<br /> класса, как будто вы в школе</p>
+                        <div class="service__buttons">
+
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><button class="lesson-button">Провести
+                                    урок</button></a>
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><button class="demo-button"><img
+                                        src="/pointer.svg" alt="">Посмотреть демо</button></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-xl-7">
+                        <div class="service__cover">
+                            <img class="service__cover-desktop" src="/service-cover-desktop.png" alt="">
+                            <img class="service__cover-mobile" src="/service-cover-mobile.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped></style>
